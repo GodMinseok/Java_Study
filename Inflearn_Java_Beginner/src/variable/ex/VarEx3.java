d008dd3d975dcd1b3948376654d6f3b151f5415b
GodMinseok <mseok830@naver.com>
2025-01-17 00:56:26 +0900

    Section3. 정리

--- Inflearn_Java_Beginner/src/variable/ex/VarEx3.java
+++ Inflearn_Java_Beginner/src/variable/ex/VarEx3.java
@@ -5,6 +5,7 @@ public class VarEx3 {
         long longVar = 10000000000L;
         System.out.println(longVar);
 
+
         boolean booleanVar = true;
         System.out.println(booleanVar);
     }
