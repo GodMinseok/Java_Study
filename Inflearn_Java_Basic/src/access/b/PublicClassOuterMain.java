271be69c601fb10e92767f757f69b5f33a771bf9
GodMinseok <mseok830@naver.com>
2025-01-28 21:09:16 +0900

    Section8. 스택과 큐 자료 구조

--- Inflearn_Java_Basic/src/access/b/PublicClassOuterMain.java
+++ Inflearn_Java_Basic/src/access/b/PublicClassOuterMain.java
@@ -10,6 +10,5 @@ public class PublicClassOuterMain {
         //다른 패키지 접근 불가
         //DefaultClass1 class1 = new DefaultClass1();
         //DefaultClass2 class2 = new DefaultClass2();
-
     }
 }
